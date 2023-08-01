b56a401cbaff0d23548f3a612b29487a6e0b53e0
gabrielcalmon <jgabrielcalmon1@hotmail.com>
2023-08-01 11:32:40 -0300

    implement 'sucess' checker system

--- src/get_transform_client.cpp
+++ src/get_transform_client.cpp
@@ -45,15 +45,12 @@ int main(int argc, char **argv)
   auto result = result_future.get();
 
   // referencia: https://github.com/ros2/examples/blob/rolling/rclcpp/services/minimal_client/main.cpp
-  // RCLCPP_INFO(
-  //   node->get_logger(), "\nR=" "\n\t%.3f\t"  "%.3f\t" "%.3f" "\n\t%.3f\t"  "%.3f\t" "%.3f" "\n\t%.3f\t"  "%.3f\t" "%.3f\n",
-  //   result->r[0], result->r[1], result->r[2], result->r[3], result->r[4], result->r[5], result->r[6], result->r[7], result->r[8]);
-
+ 
   if(result->sucess){
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Result\nPoint B:[ %f %f %f]",
-              result->point_b[0], result->point_b[1], result->point_b[2]);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "%s\nResult:\nPoint B = [ %f %f %f]",
+              result->message.c_str(),result->point_b[0], result->point_b[1], result->point_b[2]);
   } else {
-    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "The transformation failed");
+    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "The transformation failed\n%s", result->message.c_str());
   }
   
 
--- src/get_transform_server.cpp
+++ src/get_transform_server.cpp
@@ -9,18 +9,26 @@
 void transform(const std::shared_ptr<apply_transform::srv::GetTransform::Request> request,
           std::shared_ptr<apply_transform::srv::GetTransform::Response>     response)
 {
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\nz1: %f" " y: %f" " z2: %f",
-              request->point_a[0], request->point_a[1], request->point_a[2]);
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\nPoint A = [%f %f %f]"  " Theta: %f",
+              request->point_a[0], request->point_a[1], request->point_a[2], request->theta);
   
-  // double z1 = (request->z1)*M_PI/180;	// recebe o valor em graus e converte em radianos
-  // double y = (request->y)*M_PI/180;
-  // double z2 = (request->z2)*M_PI/180;
+  double theta_rad = (request->theta)*M_PI/180;
+
+  double px = request->point_a[0];
+  double py = request->point_a[1];
+  double pz = request->point_a[2];
   
-  // sequencia de preenchimento: da primeira linha, para a segunda e, por fim, para a terceira
-  response->point_b[0] = 1;
-  response->point_b[1] = 0;
-  response->point_b[2] = 0;
-  response->sucess = true;  
+  if(request->theta < 0 || request->theta > 90){
+    response->sucess = false; 
+    response->message = "The transformation failed due to invalid angle input\nTheta angle must be between 0 and 90 degrees";
+    return;
+  }
+  response->sucess = true;
+  response->message = "The transformation was completed successfully";
+  response->point_b[0] = px + 1;
+  response->point_b[1] = cos(theta_rad)*py - sin(theta_rad)*pz + 2;
+  response->point_b[2] = sin(theta_rad)*py + cos(theta_rad)*pz + 3;
+   
 
   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response");
 
